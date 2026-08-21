5f8dfd4733f42e6941e816fee32c50b7ce572b42
Ke <302652965+Ke9318@users.noreply.github.com>
2026-08-22 01:38:54 +0900

    chore: bump userscript to 1.15.1-stable

--- lanis.user.js
+++ lanis.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         lanis
 // @namespace    lanis
-// @version      1.15.0-stable
+// @version      1.15.1-stable
 // @description  재전직 / 자동사냥 / 레어맵 / 던전 / 아레나 / 심층던전 / 개인 보스 / 일일 연속 자동화를 하나의 패널에서 제공하며 각 모듈의 실행 로직은 독립적으로 격리.
 // @match        https://lanis.me/*
 // @run-at       document-idle
